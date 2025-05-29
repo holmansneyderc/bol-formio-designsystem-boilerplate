f73d70e9073ae0bc7c06e9396df922471f2b8d83
holman sneyder <holman.cabezas@segurosbolivar.com>
2025-05-29 04:05:56 -0600

    app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormioModule } from '@formio/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormioModule],
+  imports: [CommonModule, FormioModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -14,46 +14,29 @@ export class App {
   beneficios = [
     {
       titulo: 'Ahorro',
-      icono: 'assets/ahorro-icon.svg',
+      icono: 'assets/logo.svg',
       descripcion: 'Podrá construir un capital ahorrando, en un período mínimo de 12, 18, 24 y 36 meses.'
     },
     {
       titulo: 'Sorteos únicos',
-      icono: 'assets/sorteo-icon.svg',
+      icono: 'assets/logo.svg',
       descripcion: `Participe en sorteos mensuales por pagar puntualmente. <br><a href="#" class="text-green fw-bold">Conozca los ganadores</a>`
     },
     {
       titulo: 'Premios',
-      icono: 'assets/premios-icon.svg',
+      icono: 'assets/logo.svg',
       descripcion: 'Obtendrá premios en dinero equivalentes a múltiples de su cuota mensual.'
     },
     {
       titulo: 'Sorteos',
-      icono: 'assets/calendario-icon.svg',
+      icono: 'assets/logo.svg',
       descripcion: `<a href="#" class="text-green fw-bold">Conozca aquí</a> el calendario de sorteos`
     }
   ];
 
   formJson = {
     components: [
-      {
-        type: 'textfield',
-        key: 'name',
-        label: 'Nombre completo',
-        input: true
-      },
-      {
-        type: 'email',
-        key: 'email',
-        label: 'Correo electrónico',
-        input: true
-      },
-      {
-        type: 'button',
-        action: 'submit',
-        label: 'Enviar',
-        theme: 'primary'
-      }
+      
     ]
   };
 }

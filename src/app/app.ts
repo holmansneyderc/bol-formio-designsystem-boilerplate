60713175bd416887975d1e906caf78c9ec56908f
holman sneyder <holman.cabezas@segurosbolivar.com>
2025-05-29 03:51:06 -0600

    form

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,50 @@ import { FormioModule } from '@formio/angular';
 })
 export class App {
   protected title = 'formio-modular-app';
+
+  beneficios = [
+    {
+      titulo: 'Ahorro',
+      icono: 'assets/ahorro-icon.svg',
+      descripcion: 'Podrá construir un capital ahorrando, en un período mínimo de 12, 18, 24 y 36 meses.'
+    },
+    {
+      titulo: 'Sorteos únicos',
+      icono: 'assets/sorteo-icon.svg',
+      descripcion: `Participe en sorteos mensuales por pagar puntualmente. <br><a href="#" class="text-green fw-bold">Conozca los ganadores</a>`
+    },
+    {
+      titulo: 'Premios',
+      icono: 'assets/premios-icon.svg',
+      descripcion: 'Obtendrá premios en dinero equivalentes a múltiples de su cuota mensual.'
+    },
+    {
+      titulo: 'Sorteos',
+      icono: 'assets/calendario-icon.svg',
+      descripcion: `<a href="#" class="text-green fw-bold">Conozca aquí</a> el calendario de sorteos`
+    }
+  ];
+
+  formJson = {
+    components: [
+      {
+        type: 'textfield',
+        key: 'name',
+        label: 'Nombre completo',
+        input: true
+      },
+      {
+        type: 'email',
+        key: 'email',
+        label: 'Correo electrónico',
+        input: true
+      },
+      {
+        type: 'button',
+        action: 'submit',
+        label: 'Enviar',
+        theme: 'primary'
+      }
+    ]
+  };
 }
